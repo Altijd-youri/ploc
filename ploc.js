7e60b088224e8ce80f8f6d8bf75d0b9266c2e806
Altijd_Youri <advertentiey@vanderstar.eu>
2022-03-02 14:30:03 +0100

    Created support for BODY keyword, prevent files w/out and added parent names.

--- ploc.js
+++ ploc.js
@@ -26,9 +26,18 @@ ploc.utils.getAnchor = function (name) {
 ploc.getDocData = function (code) {
 
   // We need to work on a reversed string to avoid to fetch too much text, so the keywords for package, function and so on are looking ugly...
-  var regexItem = /\/\*{2,}\s*((?:.|\s)+?)\s*\*{2,}\/\s*((?:.|\s)*?\s*([\w$#]+|".+?")(?:\.(?:[\w$#]+|".+?")){0,1}\s+(reggirt|epyt|erudecorp|noitcnuf|egakcap))\s*(?:(?:ECALPER\s+RO\s+){0,1}ETAERC){0,1}\s*$/gim;
+  var regexItem = /(?:\/\*{2,}\s*((?:.|\s)+?)\s*\*{2,}\/)\s*((?:.|\s)*?\s*([\w$#]+|".+?")(\.(?:[\w$#]+|".+?")){0,1}(\s+ydob){0,1}\s+(reggirt|epyt|erudecorp|noitcnuf|egakcap))\s*(?:(?:ECALPER\s+RO\s+){0,1}ETAERC){0,1}\s*$/gim
+  /*
+  - $1: description     (undefined | 'description of function' )
+  - $2: code signature
+  - $3: name
+  - $4: parent          (undefined | 'nameOfParent.' )
+  - $5: secondary type  (undefined | body )
+  - $6: type            (package | function | procedure | type | trigger )
+  */
   var regexHeaderSetext = /(^(?:\s*(?:\r\n|\n|\r))* {0,3}\S.+(?:\r\n|\n|\r) {0,3}=+ *)(?:\r\n|\n|\r)/;
   var regexHeaderAtx = /(^(?:\s*(?:\r\n|\n|\r))* {0,3}# +.+)(?:\r\n|\n|\r)/;
+  var regexHeaderTop = /(?:\r\n|\n|\r){0,1}(.+)(?:\r\n|\n|\r)={3,}/;
   var regexLeadingWhitespace = /^(?:\s*(?:\r\n|\n|\r))*/;
   var match;
   var anchors = [];
@@ -40,20 +49,23 @@ ploc.getDocData = function (code) {
 
   // Get base attributes.
   if (!regexItem.test(code)) {
-    console.warn('PLOC: Document contains no code to process!');
+    if (args.debug) console.warn('PLOC: Document contains no code to process!');
   } else {
     // Reset regexItem index to find all occurrences with exec.
     // Also see: https://www.tutorialspoint.com/javascript/regexItem_lastindex.htm
     regexItem.lastIndex = 0;
     while (match = regexItem.exec(code)) {
       var item = {};
-      item.description = ploc.utils.reverseString(match[1])
+      item.description = (match[1] === undefined) ? '' : ploc.utils.reverseString(match[1]);
+      item.description
         .replace(/{{@}}/g, '@')   // Special SQL*Plus replacements. SQL*Plus is reacting on those special
         .replace(/{{#}}/g, '#')   // characters when they occur as the first character in a line of code.
         .replace(/{{\/}}/g, '/'); // That can be bad when you try to write Markdown with sample code.
       item.signature = ploc.utils.reverseString(match[2]);
       item.name = ploc.utils.reverseString(match[3]);
-      item.type = ploc.utils.capitalizeString(ploc.utils.reverseString(match[4]));
+      item.parent = (match[4] === undefined) ? '' : ploc.utils.reverseString(match[4]);
+      item.secondarytype = (match[5] === undefined) ? '' : ploc.utils.reverseString(match[5]);
+      item.type = ploc.utils.capitalizeString(ploc.utils.reverseString(match[6]));
       data.items.push(item);
     }
   }
@@ -74,11 +86,21 @@ ploc.getDocData = function (code) {
         data.items[i].description = data.items[i].description
           .replace(regexHeaderAtx, '')
           .replace(regexLeadingWhitespace, '');
+      } else if (match = regexHeaderTop.exec(data.items[i].description)) {
+        data.header = match[1];
+        data.items[i].description = data.items[i].description
+          .replace(regexHeaderTop, '')
+          .replace(regexLeadingWhitespace, '');
       }
     }
 
     // Define item header and anchor for TOC.
-    data.items[i].header = data.items[i].type + ' ' + data.items[i].name;
+    if (data.items[i].secondarytype !== '') {
+      data.items[i].header = data.items[i].type + ' ' + data.items[i].parent + data.items[i].name;
+    } else {
+      data.items[i].header = data.items[i].type + ' ' + data.items[i].secondarytype + data.items[i].parent + data.items[i].name;
+
+    }
     data.items[i].anchor = ploc.utils.getAnchor(data.items[i].header);
     // Ensure unique anchors.
     if (anchors.indexOf(data.items[i].anchor) !== -1) {
@@ -108,6 +130,8 @@ ploc.getDoc = function (code) {
   var doc = '';
   var docData = ploc.getDocData(code);
   var provideToc = (docData.items.length >= ploc.opts.minItemsForToc);
+
+  if(docData.items.length == 0) return '';
 
   doc += (docData.header ? docData.header + '\n\n' : '');
   doc += (provideToc ? docData.toc + '\n\n' : '');
